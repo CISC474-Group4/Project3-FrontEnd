8a7b274f507a0d7e235e751b176a4d7e04b2013c
Zendelle Badiang <Zendelle@udel.edu>
2020-04-22 14:04:20 -0400

    generate components home and Events

--- Project3-Frontend/src/app/app.module.ts
+++ Project3-Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
