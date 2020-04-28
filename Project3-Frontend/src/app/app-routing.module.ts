4097cb43db50f209982ca8556b53ac81de67cc3e
Zendelle Badiang <Zendelle@udel.edu>
2020-04-28 12:47:59 -0400

    Installed Boostrap, Popper, and Jquery dependencies to get rid of CDN. Fixed the menu so it's not on top of page component. Started Restaurant page Layout

--- Project3-Frontend/src/app/app-routing.module.ts
+++ Project3-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
+
 
 
 const routes: Routes = [
